e8fc507ffd2120b90263a7ff6f32da666fadec0e
MC <mchadha716@gmail.com>
2021-02-26 00:23:48 -0500

    changed names

---
+++ bullsAndCows/lib/bullsAndCows/application.ex
@@ -0,0 +1,34 @@
+defmodule BullsAndCows.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      BullsAndCowsWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: BullsAndCows.PubSub},
+      # Start the Endpoint (http/https)
+      BullsAndCowsWeb.Endpoint,
+      # Start a worker by calling: BullsAndCows.Worker.start_link(arg)
+      # {BullsAndCows.Worker, arg}
+      BullsAndCows.BackupAgent,
+      BullsAndCows.GameSup,
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: BullsAndCows.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    BullsAndCowsWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
--- hangman/lib/hangman_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule HangmanWeb.PageController do
-  use HangmanWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
